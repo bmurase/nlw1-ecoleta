67af9a77ba622559a71dc6862530f960fb9a01f7
Beatriz Murase <biamurase@gmail.com>
2020-06-04 18:41:47 -0300

    feat: add fonts and navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { AppLoading } from 'expo';
 import { StatusBar } from 'react-native';
 
-import Home from './src/pages/Home';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold } from '@expo-google-fonts/ubuntu';
+
+import Routes from './src/routes';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
-      <StatusBar />
-      <Home />
+      <StatusBar
+        backgroundColor="transparent"
+        barStyle="dark-content"
+        translucent
+      />
+      <Routes />
     </>
   );
 };
