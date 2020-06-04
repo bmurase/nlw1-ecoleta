d1672df8486f942a191d0d1bcfe98e0b237e7da6
Beatriz Murase <biamurase@gmail.com>
2020-06-04 18:42:32 -0300

    feat: add home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useCallback } from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, ImageBackground, StyleSheet, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Home: React.FC = () => {
-  return <View />;
-};
+  const navigation = useNavigation();
 
-export default Home;
+  const handleNavigateToPoints = useCallback(() => {
+    navigation.navigate('Points');
+  }, [navigation]);
+
+  return (
+    <ImageBackground
+      source={require('../../assets/home-background.png')}
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos.</Text>
+        <Text style={styles.description}>
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+        </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -75,3 +107,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
+
+export default Home;
