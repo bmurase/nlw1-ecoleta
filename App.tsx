2adcb44713e0faeae01fc6bd93cd11c86dda4810
Beatriz Murase <biamurase@gmail.com>
2020-06-04 15:54:18 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import Home from './src/pages/Home';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar />
+      <Home />
+    </>
+  );
+};
+
+export default App;
